0aa6c53f1dcd50b8997e16c1cabe1738235fef33
Tomas Birkantas <tomas.birkantas@gmail.com>
2018-06-27 19:20:30 +0300

    Updated project description and formatting

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Sixth-Card
-Card game (HTML/JS/CSS)
+# Sixth Card
+
+It is a simple card game created with a goal to put my Javascript knowledge into practice. For this purpose game has been written in plain Vanilla Javascript. To create this exercise project beside basic programming concepts I have used some functional programming paradigm ideas, recursive function calls, few ES6 features (arrow functions, promise for animate motion with css transition).
+
+Live version: [https://pilkas.github.io/Sixth-Card/](https://pilkas.github.io/Sixth-Card/)
+
+To do:
+- Add ES linter
+- Add Babel JS compiler
--- script.js
+++ script.js
@@ -3,22 +3,22 @@ const container = document.querySelector('#container');
 document.querySelector('#start').addEventListener('click', () => {
   fadeOut(rules, 500);
 
-  setTimeout(function(){
+  setTimeout(function () {
     newRound();
     fadeIn(container, 500);
   }, 1000);
 });
 function fadeOut(el, duration) {
-  var s = el.style, step = 25/(duration || 300);
+  var s = el.style, step = 25 / (duration || 300);
   s.opacity = s.opacity || 1;
   (function fade() { (s.opacity -= step) < 0 ? s.display = "none" : setTimeout(fade, 25); })();
 }
 
 function fadeIn(el, duration, display) {
-  var s = el.style, step = 25/(duration || 300);
+  var s = el.style, step = 25 / (duration || 300);
   s.opacity = s.opacity || 0;
   s.display = display || "block";
-  (function fade() { (s.opacity = parseFloat(s.opacity)+step) > 1 ? s.opacity = 1 : setTimeout(fade, 25); })();
+  (function fade() { (s.opacity = parseFloat(s.opacity) + step) > 1 ? s.opacity = 1 : setTimeout(fade, 25); })();
 }
 
 // window.onload = function() {
@@ -30,25 +30,25 @@ let computerCards, personCards;
 document.querySelector('#selected-cards').addEventListener('dragover', dragover_handler);
 document.querySelector('#selected-cards').addEventListener('drop', drop_handler);
 
-function isPersonsCard(cardNumber){
+function isPersonsCard(cardNumber) {
   return personCards.includes(parseInt(cardNumber));
 }
 
-function dealCards(requestedCardQty){
+function dealCards(requestedCardQty) {
   function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
   }
-  function checkIfUnique(cardNumber){
-    return (cardsInUse.find((element) => {return element === cardNumber}) === undefined)? true : false;
+  function checkIfUnique(cardNumber) {
+    return (cardsInUse.find((element) => { return element === cardNumber }) === undefined) ? true : false;
   }
 
   const pickedCards = [];
 
   while (pickedCards.length < requestedCardQty) {
     const cardNumber = getRandomIntInclusive(1, 100)
-    if(checkIfUnique(cardNumber, cardsInUse)){
+    if (checkIfUnique(cardNumber, cardsInUse)) {
       cardsInUse.push(cardNumber);
       pickedCards.push(cardNumber);
     }
@@ -57,7 +57,7 @@ function dealCards(requestedCardQty){
   return pickedCards;
 }
 
-function drawCard(cardNumber){
+function drawCard(cardNumber) {
   const newCard = document.createElement('div');
   newCard.classList.add('card');
   newCard.id = 'card-' + cardNumber; //(parseInt(document.querySelectorAll('.card').length) + 1);
@@ -67,9 +67,9 @@ function drawCard(cardNumber){
   return newCard;
 }
 
-function showCards(){
+function showCards() {
   const rowOnTable = document.querySelectorAll('.row');
-  for (let i = 0; i < rowOnTable.length; i++){
+  for (let i = 0; i < rowOnTable.length; i++) {
     dealCards(1).forEach(card => rowOnTable[i].appendChild(drawCard(card)));
   };
 
@@ -78,19 +78,19 @@ function showCards(){
   });
 }
 
-function getComputerCard(){
+function getComputerCard() {
   let bestOption;
   const rows = Array.from(document.querySelectorAll('.row'));
-  const rowWithLeastCards = rows.find((row) => {row.childElementCount === Math.min.apply(null, rows.map((row) => {return row.childElementCount;})); return row;})
+  const rowWithLeastCards = rows.find((row) => { row.childElementCount === Math.min.apply(null, rows.map((row) => { return row.childElementCount; })); return row; })
   const lastCardInRowNumber = parseInt(rowWithLeastCards.lastChild.dataset.cardNumber);
 
   for (let i = 0, minDiff; i < computerCards.length; i++) {
-    if(getSuitableRow(computerCards[i]) == null){
+    if (getSuitableRow(computerCards[i]) == null) {
       bestOption = computerCards[i];
       break;
-    }else {
+    } else {
       const diff = computerCards[i] - lastCardInRowNumber;
-      if(diff < minDiff || minDiff == null) {
+      if (diff < minDiff || minDiff == null) {
         bestOption = computerCards[i];
         minDiff = diff;
       }
@@ -100,25 +100,26 @@ function getComputerCard(){
   return bestOption;
 }
 
-function getSuitableRow(cardNr){
+function getSuitableRow(cardNr) {
   const cardNumber = parseInt(cardNr);
   const rows = Array.from(document.querySelectorAll('.row'));
-  const emptyRow = rows.find(row => {return row.firstChild == null});
+  const emptyRow = rows.find(row => { return row.firstChild == null });
   let suitableRow;
 
-  if (emptyRow) {console.log('Tuščia eilutė yra');
+  if (emptyRow) {
+    console.log('Tuščia eilutė yra');
     suitableRow = emptyRow;
   } else {
     // console.log('tuščios eilutės nėra');
-    const rowsWithLowerCards = rows.filter(row => {return row.lastChild.dataset.cardNumber < cardNumber;});
-    const CardWithMinDiff = Math.max.apply(null, rowsWithLowerCards.map((row) => {return row.lastChild.dataset.cardNumber;}))
-    suitableRow = rowsWithLowerCards.find(row => {return row.lastChild.dataset.cardNumber == CardWithMinDiff;});
+    const rowsWithLowerCards = rows.filter(row => { return row.lastChild.dataset.cardNumber < cardNumber; });
+    const CardWithMinDiff = Math.max.apply(null, rowsWithLowerCards.map((row) => { return row.lastChild.dataset.cardNumber; }))
+    suitableRow = rowsWithLowerCards.find(row => { return row.lastChild.dataset.cardNumber == CardWithMinDiff; });
   }
 
   return suitableRow;
 }
 
-function dragstart_handler(e){
+function dragstart_handler(e) {
   e.dataTransfer.setData("text/html", e.target.id);
   e.dataTransfer.dropEffect = "move";
 
@@ -132,7 +133,7 @@ function dragover_handler(e) {
   // e.target.style.opacity = '0.5';
 }
 
-function drop_handler(e){
+function drop_handler(e) {
   e.preventDefault();
   const data = e.dataTransfer.getData("text/html");
 
@@ -140,15 +141,15 @@ function drop_handler(e){
   document.querySelector('#selected-cards').appendChild(document.getElementById(data));
   document.querySelector('#selected-cards').appendChild(drawCard(getComputerCard()));
 
-  console.log('Mano korta:', document.querySelectorAll('#selected-cards .card')[0].dataset.cardNumber, 'Kompo korta:',  document.querySelectorAll('#selected-cards .card')[1].dataset.cardNumber);
+  console.log('Mano korta:', document.querySelectorAll('#selected-cards .card')[0].dataset.cardNumber, 'Kompo korta:', document.querySelectorAll('#selected-cards .card')[1].dataset.cardNumber);
   setTimeout(performAction, 1000);
 }
 
-function performAction(){
-  const selectedCards = Array.from(document.querySelectorAll('#selected-cards .card')).sort((a, b) => {return parseInt(a.dataset.cardNumber) - parseInt(b.dataset.cardNumber)});
+function performAction() {
+  const selectedCards = Array.from(document.querySelectorAll('#selected-cards .card')).sort((a, b) => { return parseInt(a.dataset.cardNumber) - parseInt(b.dataset.cardNumber) });
   console.log('Action, pasirinktos kortos: ', selectedCards);
 
-  document.querySelectorAll('#hand .card').forEach((card) => {card.removeEventListener('dragstart', dragstart_handler)});
+  document.querySelectorAll('#hand .card').forEach((card) => { card.removeEventListener('dragstart', dragstart_handler) });
   if (selectedCards[0]) {
 
     const card = selectedCards[0];
@@ -156,11 +157,11 @@ function performAction(){
     const rows = Array.from(document.querySelectorAll('.row'));
 
     if (suitableRow == null) {
-      console.log('Nėra kur padėti kortos');      
+      console.log('Nėra kur padėti kortos');
       if (isPersonsCard(card.dataset.cardNumber)) {
         console.log('Žaidėjas renkasi kurią eilutę pasiimti');
         // document.querySelectorAll('#hand .card').forEach((card) => {card.removeEventListener('dragstart', dragstart_handler)});
-        function selectRow(e){
+        function selectRow(e) {
           const row = e.currentTarget;
           takeCards(row, isPersonsCard(card.dataset.cardNumber));
           rows.forEach((row) => {
@@ -175,9 +176,9 @@ function performAction(){
         });
         return;
       } else {
-        console.log('Kompiuteris renkasi kurią eilutę pasiimti');        
-        const rowWithLeastCards = Math.min.apply(null, rows.map((row) => {return row.childElementCount;}))
-        const bestRowToTake = rows.find(row => {if(row.childElementCount == rowWithLeastCards){return row;}});
+        console.log('Kompiuteris renkasi kurią eilutę pasiimti');
+        const rowWithLeastCards = Math.min.apply(null, rows.map((row) => { return row.childElementCount; }))
+        const bestRowToTake = rows.find(row => { if (row.childElementCount == rowWithLeastCards) { return row; } });
         takeCards(bestRowToTake, isPersonsCard(card.dataset.cardNumber));
         return;
       }
@@ -186,7 +187,7 @@ function performAction(){
         console.log('Ups! Tinkamoje eilutėje 5 kortos, reikia susirinkti kortas');
         takeCards(suitableRow, isPersonsCard(card.dataset.cardNumber));
         return;
-      } else if (parseInt(suitableRow.childElementCount) < 5)  {
+      } else if (parseInt(suitableRow.childElementCount) < 5) {
         animateMotion(card, suitableRow.lastChild || suitableRow).then(
           function () {
             console.log('Dedu', card.dataset.cardNumber, ' į tinkamą eilutę: ', suitableRow);
@@ -198,13 +199,13 @@ function performAction(){
     }
   } else {
     console.log('Ėjimas baigtas');
-    document.querySelectorAll('#hand .card').forEach((card) => {card.addEventListener('dragstart', dragstart_handler)});
-    checkGameStatus(); // ar tikrai reikia?
+    document.querySelectorAll('#hand .card').forEach((card) => { card.addEventListener('dragstart', dragstart_handler) });
+    checkGameStatus();
     return;
   }
 }
 
-function takeCards(row, isPerson){
+function takeCards(row, isPerson) {
   const cards = row.querySelectorAll('.card');
   if (cards[0]) {
     if (isPerson) {
@@ -233,46 +234,46 @@ function animateMotion(a, b) {
   return new Promise((resolve) => {
     function offset(el) {
       let rect = el.getBoundingClientRect(),
-      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
       return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
     }
-    function transitionEnded () {
+    function transitionEnded() {
       a.removeEventListener('transitionend', transitionEnded);
       resolve();
     }
-    
+
     const x = offset(b).left - offset(a).left + 80;
     const y = offset(b).top - offset(a).top;
-    a.addEventListener ('transitionend', transitionEnded)
-    a.style.transform = 'translate('+ x + 'px,'+ y + 'px)';
+    a.addEventListener('transitionend', transitionEnded)
+    a.style.transform = 'translate(' + x + 'px,' + y + 'px)';
   })
 }
 
 function incrementPoints(isPerson) {
-  const playerPoints = isPerson? document.querySelector('#person-points') : document.querySelector('#pc-points');
-  playerPoints.textContent = parseInt(playerPoints.textContent)+1;
+  const playerPoints = isPerson ? document.querySelector('#person-points') : document.querySelector('#pc-points');
+  playerPoints.textContent = parseInt(playerPoints.textContent) + 1;
 }
 
-function isEnoughPoints(){
+function isEnoughPoints() {
   const personPoints = parseInt(document.querySelector('#person-points').textContent);
   const pcPoints = parseInt(document.querySelector('#pc-points').textContent);
 
   if (personPoints >= 10 || pcPoints >= 10) {
     return true;
-  }else {
+  } else {
     return false;
   }
 }
 
 function checkGameStatus() {
-  if(document.querySelector('#hand').childElementCount == 0) {
+  if (document.querySelector('#hand').childElementCount == 0) {
     document.querySelector('#container').style.opacity = 0.5;
     if (isEnoughPoints()) {
 
       const personPoints = parseInt(document.querySelector('#person-points').textContent);
       const computerPoints = parseInt(document.querySelector('#pc-points').textContent);
-      const winner = (personPoints < computerPoints)? 'You won!' : 'Computer won!';
+      const winner = (personPoints < computerPoints) ? 'You won!' : 'Computer won!';
 
       const message = `
       <h2>Game over!</h2>
@@ -289,15 +290,15 @@ function checkGameStatus() {
       <h2>Round over</h2>
       <p>Keep up!</p>
       `;
-      showPopUp(message,'Next round');
+      showPopUp(message, 'Next round');
       endRound();
     }
   }
 }
 
 function newRound() {
-  computerCards = dealCards(10).sort((a, b) => {return a - b;});
-  personCards = dealCards(10).sort((a, b) => {return a - b;});
+  computerCards = dealCards(10).sort((a, b) => { return a - b; });
+  personCards = dealCards(10).sort((a, b) => { return a - b; });
 
   console.log(computerCards);
   showCards();
@@ -322,7 +323,7 @@ function endRound() {
   })
 }
 
-function showPopUp(message, btnText){
+function showPopUp(message, btnText) {
   const popUp = document.createElement('div');
   const btn = document.createElement('button');
 
